88516455a69aa1d94747e34849bb399d275e8d4f
Alice Davis <alicedavis@killerrabbit.xyz>
2026-08-31 20:36:53 -0700

    hopefully fixed gap and window positioning

--- src/layout.rs
+++ src/layout.rs
@@ -34,19 +34,19 @@ impl Layout for MasterStack {
         let main_rect = Rect {
             x: area.x,
             y: area.y,
-            width: area.width / 2 - gap_size,
+            width: (area.width / 2).saturating_sub(gap_size),
             height: area.height
         };
         let remaining_rect = Rect {
             x: main_rect.width + main_rect.x + gap_size,
             y: area.y,
-            width: area.width - (main_rect.width + gap_size),
+            width: area.width.saturating_sub(main_rect.width + gap_size),
             height: area.height,
         };
         let mut out = Vec::with_capacity(windows.len());
         out.push(main_rect);
 
-        let part_size = area.height / (windows.len() - 1) as i32 - gap_size; // Skipping first window
+        let part_size = area.height / (windows.len() - 1) as i32; // Skipping first window
         let stack_rect = Rect {
             x: remaining_rect.x,
             y: remaining_rect.y,
@@ -56,10 +56,11 @@ impl Layout for MasterStack {
         let mut stack = vec![stack_rect; windows.len() - 1];
 
         for i in 1..stack.len() {
+            stack[i].y = stack[i - 1].y + stack[i - 1].height;
             if i != 1 {
                 stack[i].y += gap_size;
+                stack[i].height = stack[i].height.saturating_sub(gap_size);
             }
-            stack[i].y = stack[i - 1].y + stack[i - 1].height;
         }
         out.extend(stack);
 
